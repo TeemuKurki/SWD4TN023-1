182299aeda4e1b22522bb1e094d93d216857ae5b
Kurki Teemu <a1503065@myy.haaga-helia.fi>
2017-02-02 15:26:49 +0200

    added bookstore

---
+++ Bookstore/src/main/java/com/kurki/teknologiat/controller/BookController.java
@@ -0,0 +1,8 @@
+package com.kurki.teknologiat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/index")
+public class BookController {
+
+}
---
+++ Bookstore/src/main/java/com/kurki/teknologiat/model/Book.java
@@ -0,0 +1,68 @@
+package com.kurki.teknologiat.model;
+
+public class Book {
+	private String title, author, isbn, year, price;
+
+	public Book() {
+		super();
+	}
+
+	public Book(String title, String author, String isbn, String year,
+			String price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.year = year;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", isbn=" + isbn
+				+ ", year=" + year + ", price=" + price + "]";
+	}
+	
+	
+
+}

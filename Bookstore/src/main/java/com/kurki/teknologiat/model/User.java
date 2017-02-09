41e4e81ccbafa4e44fc6168c9feaaf44873df4ee
Kurki Teemu <a1503065@myy.haaga-helia.fi>
2017-02-09 17:08:51 +0200

    added User and CrudRepository

---
+++ Bookstore/src/main/java/com/kurki/teknologiat/model/User.java
@@ -0,0 +1,81 @@
+package com.kurki.teknologiat.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", updatable = false)
+	private Long id;
+	
+	@Column(name = "username", unique = true)
+	private String username;
+	
+	private String password, email, role;
+	
+	public User() {
+		super();
+	}
+
+	public User(String username, String password, String email, String role,
+			Long id) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.role = role;
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password
+				+ ", email=" + email + ", role=" + role + ", id=" + id + "]";
+	}
+	
+}
---
+++ Bookstore/src/main/java/com/kurki/teknologiat/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kurki.teknologiat.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.kurki.teknologiat.model.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}

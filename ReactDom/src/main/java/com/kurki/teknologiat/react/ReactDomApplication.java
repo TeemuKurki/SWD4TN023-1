f9aadc208bf6b06080765d178c923a0311ace3c5
Kurki Teemu <a1503065@myy.haaga-helia.fi>
2017-03-09 16:58:38 +0200

    Added react + spring project

---
+++ ReactDom/src/main/java/com/kurki/teknologiat/react/ReactDomApplication.java
@@ -0,0 +1,12 @@
+package com.kurki.teknologiat.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactDomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactDomApplication.class, args);
+	}
+}

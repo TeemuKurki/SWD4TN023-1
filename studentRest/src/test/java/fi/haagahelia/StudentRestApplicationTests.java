1a3133a60ed7ece5f98621caf9e84ae2536c8080
Kurki Teemu <a1503065@myy.haaga-helia.fi>
2017-03-16 17:33:41 +0200

    added studentRestReact

--- studentRest/src/test/java/fi/haagahelia/StudentRestApplicationTests.java
+++ studentRest/src/test/java/fi/haagahelia/StudentRestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kurki.teknologiat.react;
+package fi.haagahelia;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ReactDomApplicationTests {
+public class StudentRestApplicationTests {
 
 	@Test
 	public void contextLoads() {
